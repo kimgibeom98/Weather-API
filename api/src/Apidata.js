f2f768bd7d797079fd13a3238fe3cd7de461e675
PC <kim000183@naver.com>
2023-01-18 20:23:29 +0900

    api 데이터 수집

--- api/src/Apidata.js
+++ api/src/Apidata.js
@@ -1,44 +1,55 @@
 import axios from "axios"
 
-// nodejs vs 브라우저 런타임 차이 
-// 자바스크립트 모듈
-
-// weatherapi-react/api -> express json-server fs
-// weatherapi-react/app -> (html css js) 브라우저 html css js
-// api app
-// 스케줄러 vs setInterval
-// HMR Hot replace moudle 
-// nodejs env 
-
-let data = [];
-function Apidata() {
-  const getData = async () => {
-    const city = "Seoul";
-    const url = `${process.env.REACT_APP_BASE_URL}weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
-
-    const response = await axios.get(url);
-    data = response.data;
+const getData = async (apiUrl) => {
+  try {
+    const response = await axios.get(apiUrl);
+    const data = await response.data;
     return data
-  }
-  // console.log(123)
-  // 1 2 3 4 5 6
-  // node-schuldule
-  // 
-  const postData = async () => {
-    axios.post("http://localhost:3000/weather", {
-      temperature: data.main.temp,
-      main: data.weather[0].main,
-      icon: data.weather[0].icon,
-      speed: data.wind.speed,
-      humidity: data.main.humidity
-    })
-  }
-  postData();
 
+  } catch (err) {
+    console.log(err)
+  }
+}
+const resultData = await getData(`https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=40f1249c461da5a83fd2efe34754bdba`);
+const intervalData = await getData(`https://api.openweathermap.org/data/2.5/forecast?q=Seoul&appid=40f1249c461da5a83fd2efe34754bdba`);
+
+const postData = async () => {
+  try {
+    const data = {
+      id : resultData.weather[0].id,
+      temperature: resultData.main.temp,
+      main: resultData.weather[0].main,
+      icon: resultData.weather[0].icon,
+      speed: resultData.wind.speed,
+      humidity: resultData.main.humidity
+    }
+    const response = await axios.post('http://localhost:4000/realtime', data);
+    return response;
+
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-// getLastUpdateDate  -> (1시간넘었다,)   -> getData -> PostData
+const postIntervalData =  async () => {
+  try {
+    const data = [];
+    for(let i = 6; i < intervalData.list.length; i++){
+      data.push({
+        dt : intervalData.list[i].dt_txt,
+        temperature : intervalData.list[i].main.temp,
+        main : intervalData.list[i].weather[0].main,
+        icon : intervalData.list[i].weather[0].icon
+      })
+    }
+    const response = await axios.post('http://localhost:4000/timeinterval', data);
+    return response;
 
-Apidata();
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-// export default Apidata;
+
+postData();
+postIntervalData();
