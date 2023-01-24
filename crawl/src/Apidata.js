971382abacf533fde0b2755cd5d94acceb10449d
PC <kim000183@naver.com>
2023-01-24 23:02:47 +0900

    완료

--- crawl/src/Apidata.js
+++ crawl/src/Apidata.js
@@ -1,58 +1,60 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
 import axios from "axios"
+import schedule from 'node-schedule'
+
+dotenv.config();
 
 const city = 'Seoul';
 const host = "http://localhost:4001";
-// let seoulWeatherInfo;
-// let hourlyWeather ;
 
 const url = {
   realtime: `${process.env.BASE_URL}weather?q=${city}&appid=${process.env.API_KEY}`,
   timeinterval: `${process.env.BASE_URL}forecast?q=${city}&appid=${process.env.API_KEY}`
 }
 
-const getData = async (apiUrl) => {
+const realtimeGetdata = async () => {
   try {
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(url.realtime);
     const data = await response.data;
-    return data
-
+    putData(data)
   } catch (err) {
   }
-}
+} 
 
-const seoulWeatherInfo = await getData(url.realtime);
-const hourlyWeather = await getData(url.timeinterval);
-// const intiatlize = async() => {
-//   seoulWeatherInfo = await getData(url.realtime);
-//   hourlyWeather = await getData(url.timeinterval);
-// }
-
-const putData = async () => {
+const timeintervalGetdata = async () => {
   try {
-    const RealtimeData = {
-      temperature: seoulWeatherInfo.main.temp,
-      main: seoulWeatherInfo.weather[0].main,
-      icon: seoulWeatherInfo.weather[0].icon,
-      speed: seoulWeatherInfo.wind.speed,
-      humidity: seoulWeatherInfo.main.humidity
+    const response = await axios.get(url.timeinterval);
+    const data = await response.data;
+    console.log(data.list.length)
+    puthourlyWeather(data)
+  } catch (err) {
+  }
+} 
+
+const putData = async (data) => {
+  try {
+    const realtimeData = {
+      temperature: data.main.temp,
+      main: data.weather[0].main,
+      icon: data.weather[0].icon,
+      speed: data.wind.speed,
+      humidity: data.main.humidity
     }
-    await axios.put(`${host}/realtime/1`, RealtimeData);
+    await axios.put(`${host}/realtime/1`, realtimeData);
 
   } catch (err) {
   }
 }
 
-const puthourlyWeather = async () => {
-  const TimeintervalData = hourlyWeather.list.map((items, index) => ({
-    dt: hourlyWeather.list[index].dt_txt,
-    temperature: hourlyWeather.list[index].main.temp,
-    main: hourlyWeather.list[index].weather[0].main,
-    icon: hourlyWeather.list[index].weather[0].icon
+const puthourlyWeather = async (data) => {
+  const timeintervalData = data.list.map((item) => ({
+    dt: item.dt_txt,
+    temperature: item.main.temp,
+    main: item.weather[0].main,
+    icon: item.weather[0].icon
   }));
   try {
-    Promise.all(TimeintervalData.map(async (items,index) => {
+    Promise.all(timeintervalData.map(async (items, index) => {
       await axios.put(`${host}/timeinterval/${index + 1}`, items);
     }))
   } catch (err) {
@@ -61,5 +63,7 @@ const puthourlyWeather = async () => {
 }
 
 
-putData();
-puthourlyWeather();
+schedule.scheduleJob('* 0 * * * *', function () {
+  timeintervalGetdata();
+  realtimeGetdata();
+});
